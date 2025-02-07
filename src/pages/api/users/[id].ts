0d05bd042475298c9413e850cf084337f4f31ed8
Artem Denisov <bysama1995@gmail.com>
2025-02-07 18:17:15 +0300

    fix issues

--- src/pages/api/users/[id].ts
+++ src/pages/api/users/[id].ts
@@ -1,11 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { users } from '../../../../db/db.json'; // Предполагаем, что у вас есть файл с данными
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function handler(req: NextApiRequest, res: any) {
     const { id } = req.query; // Получаем идентификатор пользователя из URL
 
     // Добавляем заголовки CORS
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', '*'); // Замените '*' на конкретный домен в продакшн среде
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
     res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -3,9 +3,9 @@ import data from '../../../../db/db.json';
 
 let users = data.users;
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function handler(req: NextApiRequest, res: any) {
     // Добавляем заголовки CORS
-    res.setHeader('Access-Control-Allow-Credentials', "true");
+    res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', '*'); // Замените '*' на конкретный домен в продакшн среде
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
     res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
