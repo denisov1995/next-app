0aa797b453be5255c750e3f40fa2a93604c162b4
Artem Denisov <bysama1995@gmail.com>
2025-02-10 12:16:51 +0300

    fix

--- src/app/users/[id]/page.tsx
+++ src/app/users/[id]/page.tsx
@@ -1,6 +1,17 @@
 import BackButton from "@/app/components/BackButton";
 import { fetchUserData } from "../../lib/fetchUserData";
+import { log } from "node:console";
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+async function fetchAllUsers(): Promise<any> {
+  const res = await fetch(`${apiUrl}/users`);
+  const users: any[] = (await res.json()).data.users;
+  return users;
+}
 const UserPage = async ({ params }: any) => {
+  const us = await fetchAllUsers();
+
+  console.log('us',us);
+  
   const paramsValue = await params;
   const user = await fetchUserData(paramsValue.id);
 
