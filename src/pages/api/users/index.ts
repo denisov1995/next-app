6277065607b8c9590d4ed709143c74c7431ef7af
Artem Denisov <bysama1995@gmail.com>
2025-02-10 13:08:41 +0300

    fix

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -64,8 +64,12 @@ let users = [
   }
 ];
 
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function handler(req: NextApiRequest, res: any) {
+    // Добавляем заголовки CORS
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
 
     if (req.method === 'OPTIONS') {
         res.status(200).end();
