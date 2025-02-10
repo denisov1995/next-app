a042add342a8167450dcdb9fea627465b4c6a5d0
Artem Denisov <bysama1995@gmail.com>
2025-02-10 11:15:53 +0300

    fix

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -1,7 +1,76 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import data from '../../../../db/db.json';
+import data1 from '../../../../db/db.json';
+import { log } from 'console';
+
+
+let data = {
+    "users": [
+        {
+            "id": 1,
+            "name": "John Doe",
+            "email": "john@example.com",
+            "phone": "123-456-7890"
+        },
+        {
+            "id": 2,
+            "name": "Doe Jane",
+            "email": "jane@example.com",
+            "phone": "098-765-4321"
+        },
+        {
+            "id": 3,
+            "name": "Michael Smith",
+            "email": "michael.smith@example.com",
+            "phone": "234-567-8901"
+        },
+        {
+            "id": 4,
+            "name": "Emily Johnson",
+            "email": "emily.johnson@example.com",
+            "phone": "345-678-9012"
+        },
+        {
+            "id": 5,
+            "name": "Robert Brown",
+            "email": "robert.brown@example.com",
+            "phone": "456-789-0123"
+        },
+        {
+            "id": 6,
+            "name": "Jessica Davis",
+            "email": "jessica.davis@example.com",
+            "phone": "567-890-1234"
+        },
+        {
+            "id": 7,
+            "name": "Daniel Wilson",
+            "email": "daniel.wilson@example.com",
+            "phone": "678-901-2345"
+        },
+        {
+            "id": 8,
+            "name": "Sarah Martinez",
+            "email": "sarah.martinez@example.com",
+            "phone": "789-012-3456"
+        },
+        {
+            "id": 9,
+            "name": "David Anderson",
+            "email": "david.anderson@example.com",
+            "phone": "890-123-4567"
+        },
+        {
+            "id": 10,
+            "name": "Laura Thomas",
+            "email": "laura.thomas@example.com",
+            "phone": "901-234-5678"
+        }
+    ]
+}
 
 let users = data.users;
+
+
 
 export default function handler(req: NextApiRequest, res: any) {
     // Добавляем заголовки CORS
@@ -12,6 +81,8 @@ export default function handler(req: NextApiRequest, res: any) {
 
     // Обработка preflight-запросов
     if (req.method === 'OPTIONS') {
+        console.log(7777);
+        
         res.status(200).end();
         return;
     }
@@ -21,6 +92,7 @@ export default function handler(req: NextApiRequest, res: any) {
         users.push(newUser);
         res.status(201).json(newUser);
     } else if (req.method === 'GET') {
+        console.log('{ data: { users } }', { data: { users } });
         res.status(200).json({ data: { users } });
     } else {
         res.status(405).json({ message: 'Метод не поддерживается' });
