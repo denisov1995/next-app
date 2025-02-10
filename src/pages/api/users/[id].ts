e5e988b1641ef596fc448a8162a04e3f80833eb7
Artem Denisov <bysama1995@gmail.com>
2025-02-10 14:00:23 +0300

    fix

--- src/pages/api/users/[id].ts
+++ src/pages/api/users/[id].ts
@@ -4,18 +4,6 @@ import { users } from '@/../db/db.json'; // Предполагаем, что у 
 export default function handler(req: NextApiRequest, res: any) {
     const { id } = req.query; // Получаем идентификатор пользователя из URL
 
-    // Добавляем заголовки CORS
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Замените '*' на конкретный домен в продакшн среде
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
-
-    // Обработка preflight-запросов
-    if (req.method === 'OPTIONS') {
-        res.status(200).end();
-        return;
-    }
-
     if (req.method === 'GET') {
         const user = users.find((user: any) => user.id == id); // Находим пользователя по идентификатору
         if (user) {
