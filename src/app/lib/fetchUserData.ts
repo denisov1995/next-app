24233ff5e32e065689fd6c7aea7728894e0fe6ab
Artem Denisov <bysama1995@gmail.com>
2025-02-10 17:40:32 +0300

    fix

--- src/app/lib/fetchUserData.ts
+++ src/app/lib/fetchUserData.ts
@@ -1,4 +1,4 @@
-
+'use client';
 import { User } from "../types/User";
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 /**
@@ -8,7 +8,6 @@ const apiUrl = process.env.NEXT_PUBLIC_API_URL;
  */
 export async function fetchUserData(id: string): Promise<User | null> {
     const res = await fetch(`${apiUrl}/users/${id}`);
-    console.log('path', `${apiUrl}/users/${id}`);
 
     if (!res.ok) {
         return null;
